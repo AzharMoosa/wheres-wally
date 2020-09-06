25d9c64b15573cb4cf16b286861cf289f4e62617
AzharMoosa <azhar.moosa8@gmail.com>
2020-09-06 09:52:30 +0100

    Media Queries

--- client/src/components/game/ScoreTable.js
+++ client/src/components/game/ScoreTable.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
 import GameContext from "../../context/gameContext";
 import PlayerScore from "./PlayerScore";
 
