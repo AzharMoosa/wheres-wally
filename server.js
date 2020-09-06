68b3e3781dd96e2cb6c1c573b596fb8b1971d721
AzharMoosa <azhar.moosa8@gmail.com>
2020-09-06 10:00:52 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +12,16 @@ connectDB();
 // Define Routes
 app.use("/api/game", require("./routes/game"));
 app.use("/api/player", require("./routes/player"));
+
+// Server static assests in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
